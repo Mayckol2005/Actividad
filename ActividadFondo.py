8a3a04e83bac88860c8a127db7327cdeda713929
Mayckol2005 <169004444+Mayckol2005@users.noreply.github.com>
2024-05-05 20:09:24 -0400

    Add files via upload

---
+++ ActividadFondo.py
@@ -0,0 +1,33 @@
+import random
+
+"""
+simbolos para 
+´
+*
+"""
+#Asignamos i y j igual 0
+
+i=0;j=0;
+
+
+print("\n\n\n")
+linea="                                    "
+
+#Hacemos uso de condiciones
+
+for i in range(20):
+    linea="                                              "
+    for j in range(80):
+        random1 = random.randint(1,20)
+        if random1==1:
+            linea=linea+"*"
+        elif random1==2:
+            linea=linea+"´"
+        else:
+            linea=linea+" "
+        if j==79:
+
+#Mostramos el fondo
+            
+            print(linea)
+print("\n\n\n")
